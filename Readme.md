c781db795a5e9eb4a9f46dbce08528d8b1e8cc5b
Руслан Таранов <tortoed5@gmail.com>
2025-11-30 22:55:43 +1100

    changed the sample

--- Readme.md
+++ Readme.md
@@ -16,29 +16,34 @@ Try the [live demo](https://cakeeatergames.github.io/sunscript/)
 ## Usage
 
 ```js
-let SUN = #fs.cake.sun_v0({import:true})
-let CharCountFilter = SUN.compile(`
-	alias c = char_count
-	c[1500:*] -> keep price=10BGC;>    // Keep all char_counts with 1500+ chars 
-	c[1460:*] -> sell price=1BGC;>     // Sell all char_counts with 1460+ chars
-	c         -> cull;>                // Destroy all other char_counts
+function(context, args) {
+  try {
+    let SUN = #fs.cake.sun({ import: true })
+    let CharCountFilter = SUN.compile(`
+        alias c = char_count
+        c[1500:*] -> keep price=10BGC;>    // Keep all char_counts with 1500+ chars 
+        c[1460:*] -> sell price=1BGC;>     // Sell all char_counts with 1460+ chars
+        c         -> cull;>                // Destroy all other char_counts
 
-	--- // Next stage 
+        --- // Next stage 
 
-	c keep _best=3 -> the_best     
-	c keep !the_best -> sell keep=undefined    // Only keep the best 3
-	c -> display            // Display all char_counts 
-`)
-let upgrades = #hs.sys.upgrades({full:true})
-let filtered = SUN.filter(upgrades, CharCountFilter)
-// "filtered" will contain all items of array "upgrades" that triggered at least one rule in the list.
-// Array items now have additional properties: keep, sell, cull, price, the_best, _filtered, display
-let keep = upgrades.filter(a=>a.keep)
-let sell = upgrades.filter(a=>a.sell)
-let cull = upgrades.filter(a=>a.cull)
+        c keep _best=3 -> the_best     
+        c keep !the_best -> sell keep=undefined    // Only keep the best 3
+        c -> display            // Display all char_counts 
+      `)
+    let upgrades = #hs.sys.upgrades({ full: true })
+    let filtered = SUN.filter(upgrades, CharCountFilter)
+    // "filtered" will contain all items of array "upgrades" that triggered at least one rule in the list.
+    // Array items now have additional properties: keep, sell, cull, price, the_best, display
+    return upgrades.filter(a => a.display)
+  } catch (ex) {
+    return ex.stack
+  }
+}
+
 ```
 
-Use #fs.cake.sun_v0({import:true})
+Use #fs.cake.sun({import:true})
 Or download it from the dist folder
 
 ## Use cases for SUNSCRIPT
--- samples/test.js
+++ samples/test.js
@@ -1,60 +1,23 @@
 function(context, args) {
-
   try {
-    let clib = #fs.cake.clib({get:true});
+    let SUN = #fs.cake.sun({ import: true })
+    let CharCountFilter = SUN.compile(`
+        alias c = char_count
+        c[1500:*] -> keep price=10BGC;>    // Keep all char_counts with 1500+ chars 
+        c[1460:*] -> sell price=1BGC;>     // Sell all char_counts with 1460+ chars
+        c         -> cull;>                // Destroy all other char_counts
 
-    let logs = []
-    function log(v) {
-      if (typeof v === "object") {
-        logs.push(JSON.stringify(v));
-      } else {
-        logs.push(v);
-      }
-    }
-    let windowW = 40
-    function unlog() {
-      return clib.limitStr(clib.drawWindow(
-        {
-          text: logs.join("\n"),
-          color: "J",
-          title: "  cake.sun - Short Upgrade Notation",
-          padding: { top: 1, left: 2, right: 2, bottom: 1 },
-          margin: { top: 1, left: 4, right: 4, bottom: 1 },
-          dimensions: { width: windowW, height: 10 }
-        }
-      ))
-      return logs.join("\n");
-    }
-    function getUps() {
-      return #hs.sys.upgrades({ full: true })
-    }
+        --- // Next stage 
 
-
-    const SUN = #fs.cake.sun_v0({ import: true })
-    const CharCountFilter = SUN.compile(`
-	alias c = char_count
-	c[1500:*] -> keep price=10BGC;>    // Keep all char_counts with 1400+ chars 
-	c[1460:*] -> sell price=1BGC;>     // Sell all char_counts with 1000+ chars
-	c         -> cull;>                // Destroy all other char_counts
-
-	#1 c keep _best=3 -> the_best     
-	#1 c keep !the_best -> sell keep=undefined    // Only keep the best 3
-`)
-    let upgrades = getUps();
-    const filtered = SUN.filter(upgrades, CharCountFilter)
-
-    let cols = {
-      i:"i",
-      name:"name",
-      keep:"keep",
-      sell: "sell",
-      cull: "cull",
-      price: "price"
-    }
-    let t = clib.table(upgrades, cols)
-    log(t)
-
-    return unlog();
+        c keep _best=3 -> the_best     
+        c keep !the_best -> sell keep=undefined    // Only keep the best 3
+        c -> display            // Display all char_counts 
+      `)
+    let upgrades = #hs.sys.upgrades({ full: true })
+    let filtered = SUN.filter(upgrades, CharCountFilter)
+    // "filtered" will contain all items of array "upgrades" that triggered at least one rule in the list.
+    // Array items now have additional properties: keep, sell, cull, price, the_best, display
+    return upgrades.filter(a => a.display)
   } catch (ex) {
     return ex.stack
   }
